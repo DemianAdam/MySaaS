25f54e7d882c55fe555ebc7b7f47ff883fc5c78c
Demian <61812941+DemianAdam@users.noreply.github.com>
2025-12-15 16:48:30 -0300

    Add centralized exception handling with ProblemDetails
    Introduced a centralized exception handling mechanism using custom exception handlers for NotFoundException and global errors. Registered these handlers and ProblemDetails configuration via a new AddExceptionHandlers extension. Updated Program.cs to use the new middleware, ensuring consistent error responses and improved logging for unhandled exceptions.

---
+++ MySaaS.WebApi/Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MySaaS.WebApi.Exceptions
+{
+    internal class GlobalExceptionHandler : IExceptionHandler
+    {
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+        private readonly IProblemDetailsService _problemDetailsService;
+        public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger, IProblemDetailsService problemDetailsService)
+        {
+            _logger = logger;
+            _problemDetailsService = problemDetailsService;
+        }
+        public async ValueTask<bool> TryHandleAsync(
+            HttpContext httpContext,
+            Exception exception,
+            CancellationToken cancellationToken)
+        {
+            _logger.LogError(exception, "Unhandled exception occurred");
+
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            ProblemDetailsContext problemDetailsContext = new ProblemDetailsContext()
+            {
+                HttpContext = httpContext,
+                Exception = exception,
+                ProblemDetails = new ProblemDetails()
+                {
+                    Type = exception.GetType().Name,
+                    Detail = exception.Message,
+                }
+            };
+
+
+            return await _problemDetailsService.TryWriteAsync(problemDetailsContext);
+        }
+    }
+}
