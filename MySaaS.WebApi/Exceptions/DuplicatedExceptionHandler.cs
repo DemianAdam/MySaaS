0ec543458ce8e18194f43672a7909ad2da3dfb99
Demian <61812941+DemianAdam@users.noreply.github.com>
2025-12-15 17:55:23 -0300

    Handle DB duplicate entry errors with custom exception
    Introduce DuplicatedException to handle unique constraint violations in a structured way. Add DuplicatedExceptionHandler to return 409 Conflict responses for these errors. Update UnitService to throw DuplicatedException on PostgreSQL unique violations. Register the new handler in DI, import Npgsql for exception handling, and simplify related unit tests.

---
+++ MySaaS.WebApi/Exceptions/DuplicatedExceptionHandler.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MySaaS.Domain.Exceptions.Common;
+
+namespace MySaaS.WebApi.Exceptions
+{
+    public class DuplicatedExceptionHandler : IExceptionHandler
+    {
+        private readonly IProblemDetailsService _problemDetailsService;
+        public DuplicatedExceptionHandler(IProblemDetailsService problemDetailsService)
+        {
+            _problemDetailsService = problemDetailsService;
+        }
+        public async ValueTask<bool> TryHandleAsync(
+            HttpContext httpContext,
+            Exception exception,
+            CancellationToken cancellationToken)
+        {
+            if (exception is not DuplicatedException notFoundException)
+            {
+                return false;
+            }
+
+            httpContext.Response.StatusCode = StatusCodes.Status409Conflict;
+            ProblemDetailsContext problemDetailsContext = new ProblemDetailsContext()
+            {
+                HttpContext = httpContext,
+                Exception = exception,
+                ProblemDetails = new ProblemDetails()
+                {
+                    Type = exception.GetType().Name,
+                    Detail = exception.Message,
+                }
+            };
+            return await _problemDetailsService.TryWriteAsync(problemDetailsContext);
+        }
+    }
+}
